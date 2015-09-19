acca8ac01c425c278f2bf0a0a6f6f7aa1b1f08d8
Adam <adam.furbee@gmail.com>
2015-09-19 15:11:39 -0500

    Initial commit

---
+++ src/main/java/Questionnaire.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Adam Furbee on 9/19/2015.
+ * This will be the main request handler for the project.
+ */
+
+import static spark.Spark.*;
+
+public class Questionnaire {
+    public static void main(String[] args) {
+        port(80);
+        int maxThreads = 8;
+        threadPool(maxThreads);
+
+        // Each request spawns a thread to handle it.
+        get("/hello", (req, res) -> "Hello World");
+        return;
+    }
+}
