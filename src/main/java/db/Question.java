73a421826d5f0e37183e31e369951913aea1690d
Adam <adam.furbee@gmail.com>
2015-09-26 14:53:52 -0500

    Add question class

---
+++ src/main/java/db/Question.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of COSC3318-Questionnaire.
+ *
+ *     COSC3318-Questionnaire is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     COSC3318-Questionnaire is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package db;
+
+/**
+ * Note that this class is meant to be handled by the survey class.
+ * You can think of a question object as the question along with its choices.
+ * Type will be an enum most likely in the format TRUEFALSE,MULTIPLECHOICE,MULTIPLECHECK,OPEN
+ * or something of that nature for velocity to parse it correctly.
+ */
+public class Question {
+    final String question;
+    final String[] choice;
+    final int type;
+
+    public Question(String question, String[] choice, int type) {
+        this.question = question;
+        this.choice = choice;
+        this.type = type;
+    }
+}
