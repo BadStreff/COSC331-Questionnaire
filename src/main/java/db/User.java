997e71dc3099ce5dc7d8618013d5b70d5bda0188
corey9752 <parkercorey52@att.net>
2015-10-16 19:33:44 -0500

    Populated User class.

--- src/main/java/db/User.java
+++ src/main/java/db/User.java
@@ -1,7 +1,60 @@
 package db;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by ajf023 on 10/12/2015.
  */
 public class User {
+
+    public enum Type {
+        ADMIN, REGULAR
+    }
+
+    final String userName;
+    final String email;
+    final String password;
+    final Type type;
+    final int uId;
+
+    public User(){
+        this.userName = "";
+        this.email = "";
+        this.password = "";
+        this.type = Type.REGULAR;
+        this.uId = 0;
+    }
+
+    public User(String userName,String email,String password,Type type,int uId){
+        this.userName = userName;
+        this.email = email;
+        this.password = hashPassword(password);
+        this.type = type;
+        this.uId = uId;
+    }
+
+    private String hashPassword(String password) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.print("Unable to hash passwords, storing in plain text");
+            return password;
+        }
+        md.update(password.getBytes());
+
+        byte byteData[] = md.digest();
+
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        System.out.println("Hex format : " + sb.toString());
+        return sb.toString();
+    }
 }
+//Check if the database already exist or not
+
