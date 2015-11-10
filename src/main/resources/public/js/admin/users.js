39f1e605cd42ef9011fabaa1ff8eaf845567f04c
Adam Furbee <adam.furbee@gmail.com>
2015-11-10 13:22:50 -0600

    implemented frontend admin reset password functionality

--- src/main/resources/public/js/admin/users.js
+++ src/main/resources/public/js/admin/users.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
+    var id = "";
     $('button#delete').click( function (e) {
-        var id = $(this).parent().parent().attr('id');
+        id = $(this).parent().parent().attr('id');
          $.post('/admin/delete_user', {'username': id}).done(function(result){
                     window.location.href = '/admin/users';
                     console.log(result);
          });
+    });
+    $('button#reset').click( function (e) {
+        id = $(this).parent().parent().attr('id');
+        $("#reset_modal").modal('show');
+    });
+
+    $('button#confirm_reset').click( function (e) {
+        $.post('/change_password', {'username': id,
+                                    'password': $("input[id='new_password']").val()}).done(function(result){
+            window.location.href = '/admin/users';
+            console.log(result);
+        });
+        $("#reset_modal").modal('hide');
     });
 });
